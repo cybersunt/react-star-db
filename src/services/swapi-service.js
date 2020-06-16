7d3b008ce4decd4a0b44e450d6ee6531373e0b46
Maksimova, Yuliya <Yuliya.Maksimova@nexign.com>
2020-06-16 14:33:41 +0300

    Сделала трансформацию данных для планет

--- src/services/swapi-service.js
+++ src/services/swapi-service.js
@@ -1,41 +1,55 @@
 export default class SwapiService {
 
-    _apiBase = 'https://swapi.dev/api';
+  _apiBase = 'https://swapi.dev/api';
 
-    async getResource(url) {
-        const res = await fetch(`${this._apiBase}${url}`);
+  async getResource(url) {
+    const res = await fetch(`${this._apiBase}${url}`);
 
-        if (!res.ok) {
-            throw new Error(`Could not fetch ${url}` +
-                `, received ${res.status}`)
-        }
-        return await res.json();
+    if (!res.ok) {
+      throw new Error(`Could not fetch ${url}` +
+        `, received ${res.status}`)
     }
+    return await res.json();
+  }
 
-    async getAllPeople() {
-        const res = await this.getResource(`/people/`);
-        return res.results;
-    }
+  async getAllPeople() {
+    const res = await this.getResource(`/people/`);
+    return res.results;
+  }
 
-    getPerson(id) {
-        return this.getResource(`/people/${id}/`);
-    }
+  getPerson(id) {
+    return this.getResource(`/people/${id}/`);
+  }
 
-    async getAllPlanets() {
-        const res = await this.getResource(`/planets/`);
-        return res.results;
-    }
+  async getAllPlanets() {
+    const res = await this.getResource(`/planets/`);
+    return res.results.map(this._transformPlanet);
+  }
 
-    getPlanet(id) {
-        return this.getResource(`/planets/${id}/`);
-    }
+  async getPlanet(id) {
+    const planet = await this.getResource(`/planets/${id}/`);
+    return this._transformPlanet(planet);
+  }
 
-    async getAllStarships() {
-        const res = await this.getResource(`/starships/`);
-        return res.results;
-    }
+  async getAllStarships() {
+    const res = await this.getResource(`/starships/`);
+    return res.results;
+  }
 
-    getStarship(id) {
-        return this.getResource(`/starships/${id}/`);
-    }
+  getStarship(id) {
+    return this.getResource(`/starships/${id}/`);
+  }
+
+  _transformPlanet(planet) {
+    const idRegExp = /\/([0-9]*)\/$/;
+    const id = planet.url.match(idRegExp)[1];
+
+    return {
+      id,
+      name: planet.name,
+      population: planet.population,
+      rotationPeriod: planet.rotation_period,
+      diameter: planet.diameter
+    };
+  }
 }
