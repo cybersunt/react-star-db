3955058c8a8cb5059beef96653e4ff32804856d3
Maksimova, Yuliya <Yuliya.Maksimova@nexign.com>
2020-06-22 15:23:19 +0300

    Введение в жизненный цикл компонентов

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import Header from '../header';
 import RandomPlanet from '../random-planet';
@@ -7,11 +7,36 @@ import PersonDetails from '../person-details';
 
 import './app.css';
 
-const App = () => {
-  return (
-      <div>
+export default class App extends Component {
+
+  state = {
+    showRandomPlanet: true
+  };
+
+  toggleRandomPlanet = () => {
+    this.setState((state) => {
+      return {
+        showRandomPlanet: !state.showRandomPlanet
+      }
+    });
+  };
+
+  render() {
+
+    const planet = this.state.showRandomPlanet ?
+      <RandomPlanet/> :
+      null;
+
+    return (
+      <div className="stardb-app">
         <Header />
-        <RandomPlanet />
+        { planet }
+
+        <button
+          className="toggle-planet btn btn-warning btn-lg"
+          onClick={this.toggleRandomPlanet}>
+          Toggle Random Planet
+        </button>
 
         <div className="row mb2">
           <div className="col-md-6">
@@ -22,7 +47,6 @@ const App = () => {
           </div>
         </div>
       </div>
-  );
-};
-
-export default App;
+    );
+  }
+}
--- src/components/random-planet/random-planet.js
+++ src/components/random-planet/random-planet.js
@@ -18,6 +18,8 @@ export default class RandomPlanet extends Component {
   constructor() {
     super();
     this.updatePlanet();
+    this.interval = setInterval(this.updatePlanet, 10000);
+    // clearInterval(this.interval);
   }
 
   onPlanetLoaded = (planet) => {
@@ -34,13 +36,13 @@ export default class RandomPlanet extends Component {
     });
   };
 
-  updatePlanet() {
-    const id = 12;
+  updatePlanet = () => {
+    const id = Math.floor(Math.random()*17) + 2;
     this.swapiService
       .getPlanet(id)
       .then(this.onPlanetLoaded)
       .catch(this.onError);
-  }
+  };
 
   render() {
 
