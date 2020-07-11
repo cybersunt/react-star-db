ec1c7422778fd6fce8cabda9db52e3296786ec88
Maksimova, Yuliya <Yuliya.Maksimova@nexign.com>
2020-07-11 18:24:45 +0300

    Добавлена обработка ошибок через componentDidCatch на уровне компонета PeoplePage

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -2,24 +2,18 @@ import React, { Component } from 'react';
 
 import Header from '../header';
 import RandomPlanet from '../random-planet';
-import ItemList from '../item-list';
-import PersonDetails from '../person-details';
+import ErrorButton from '../error-button';
+import ErrorIndicator from '../error-indicator';
+import PeoplePage from '../people-page';
 
 import './app.css';
-import ErrorButton from "../error-button";
-import ErrorIndicator from "../error-indicator";
 
 export default class App extends Component {
 
   state = {
     showRandomPlanet: true,
-    selectedPerson: 5,
     hasError: false
   };
-
-  componentDidCatch() {
-    this.setState({ hasError: true });
-  }
 
   toggleRandomPlanet = () => {
     this.setState((state) => {
@@ -29,11 +23,9 @@ export default class App extends Component {
     });
   };
 
-  onPersonSelected = (id) => {
-    this.setState({
-      selectedPerson: id
-    });
-  };
+  componentDidCatch() {
+    this.setState({ hasError: true });
+  }
 
   render() {
 
@@ -56,17 +48,11 @@ export default class App extends Component {
             onClick={this.toggleRandomPlanet}>
             Toggle Random Planet
           </button>
-          <ErrorButton/>
+          <ErrorButton />
         </div>
 
-        <div className="row mb2">
-          <div className="col-md-6">
-            <ItemList onItemSelected={this.onPersonSelected}/>
-          </div>
-          <div className="col-md-6">
-            <PersonDetails personId={this.state.selectedPerson} />
-          </div>
-        </div>
+        <PeoplePage />
+
       </div>
     );
   }
